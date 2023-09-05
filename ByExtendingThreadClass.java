16c7c6080e6a9ecc7e3140b696e1752d79534699
sachin <raykumar01sachin@gmail.com>
2023-09-05 22:47:39 +0530

    My First Commit

---
+++ ByExtendingThreadClass.java
@@ -0,0 +1,25 @@
+package com.multithreading.inJava;
+class ChildThread extends Thread
+{
+	public void run()
+	{
+		System.out.println("I am being executed by child- thread and my name is " +Thread.currentThread().getName());
+		   for( int i =0; i< 5; i++)
+		   {
+		      System.out.println("Child - Thread");
+		   }
+	}
+}
+public class ByExtendingThreadClass
+{
+   public static void main(String[] args)
+   {
+	   Thread t1 = new ChildThread();
+	   t1.start();
+	   System.out.println("I am being executed by main - thread and my name is " +Thread.currentThread().getName());
+	   for( int i =0; i< 5; i++)
+	   {
+	      System.out.println("Main - Thread");
+	   }	   
+	}
+}
